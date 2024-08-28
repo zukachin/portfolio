5bcda32f4085ee120012c766b3f53de12fbda162
swetha <tswetha3188@gmail.com>
2024-08-28 14:17:00 +0530

    Update and rename script.js to app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 1, 1).normalize();
+scene.add(light);
+
+camera.position.z = 5;
